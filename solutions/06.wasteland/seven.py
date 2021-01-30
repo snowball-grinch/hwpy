0887665ac3ead6e56f3576962a864133acaef875
sadeq hashemi <sadeqmohammadh@gmail.com>
2021-01-31 03:01:17 +0330

    regard to how files should be open.

--- solutions/06.wasteland/seven.py
+++ solutions/06.wasteland/seven.py
@@ -3,7 +3,8 @@
 import re
 from one import poem_content
 
-new_file_5 = open("out_quote.txt", "w")
 quotes = re.findall(r"\".+", poem_content)
-for quote in enumerate(quotes):
-    new_file_5.write(f"{quote[1]}\n")
+
+with open("out_quote.txt", "w") as new_file_5:
+    for quote in enumerate(quotes):
+        new_file_5.write(f"{quote[1]}\n")
--- solutions/06.wasteland/six.py
+++ solutions/06.wasteland/six.py
@@ -3,7 +3,8 @@
 import re
 from one import poem_content
 
+sentences = re.finditer(r"[\".!,?]\s\"*[A-Z]", poem_content)  # Return an iterator of match objects
+num_sentences = len([*sentences]) + 2  # 2 -> 1 + 1 -> for python indexing + for considering the last sentence
+
 with open("out_sentences_count.txt", "w") as new_file4:
-    sentences = re.finditer(r"[\".!,?]\s\"*[A-Z]", poem_content)  # Return an iterator of match objects
-    num_sentences = len([*sentences]) + 2  # 2 -> 1 + 1 -> for python indexing + for considering the last sentence
     new_file4.write(str(num_sentences))
