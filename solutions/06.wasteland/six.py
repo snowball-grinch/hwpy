363daa647f76ddef2a6aa1b54344a5be4879f316
sadeq hashemi <77391151+snowball-grinch@users.noreply.github.com>
2021-02-03 17:31:44 +0330

    Update six.py
    correct the definition of one variable

--- solutions/06.wasteland/six.py
+++ solutions/06.wasteland/six.py
@@ -4,7 +4,7 @@ import re
 from one import poem_content
 
 sentences = re.finditer(r"[\".!,?]\s\"*[A-Z]", poem_content)  # Return an iterator of match objects
-num_sentences = len([*sentences]) + 2  # 2 -> 1 + 1 -> for python indexing + for considering the last sentence
+num_sentences = len([*sentences]) + 1  # 1 -> 1 -> for considering the last sentence
 
 with open("out_sentences_count.txt", "w") as new_file4:
     new_file4.write(str(num_sentences))
