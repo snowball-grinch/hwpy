4e3f15afb92aebfd9ce2892151bf73cc9f62aeac
sadeq hashemi <sadeqmohammadh@gmail.com>
2021-02-05 21:58:09 +0330

    some change

--- solutions/07.files_files_everywhere/two.py
+++ solutions/07.files_files_everywhere/two.py
@@ -6,10 +6,9 @@ for file in one.list_of_files_obj:
     set_parents.update(file.parents)
     one.File.list_of_parents.clear()
 
-if __name__ == "__main__":
-    for file in one.list_of_files_obj[0: 2]:
-        print(file.file_name)
+with open("no_children.txt", "w+") as no_children:
+    for file in one.list_of_files_obj:
+        #print(file.file_name)
         if file in set_parents:
             print(file.file_name)
-            #with open("no_children.txt", "a+") as no_children:
-            #    no_children.write(f"{file}")
+            #no_children.writelines(f"{file.file_name}")
